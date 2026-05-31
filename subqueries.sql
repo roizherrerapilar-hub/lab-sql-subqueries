71c34c4443bbee777d0ae93a8a93ccd6b8453331
roizherrerapilar-hub <roizherrerapilar@gmail.com>
2026-05-31 14:58:03 +0200

    Update subqueries.sql

--- subqueries.sql
+++ subqueries.sql
@@ -2,25 +2,27 @@ USE sakila;
 
 SELECT COUNT(*) AS number_of_copies
 FROM inventory
-WHERE film_id = (SELECT film_id FROM film WHERE title = 'HUNCHBACK IMPOSSIBLE');
-
-SELECT 
-  title,
-  length
-FROM film
-WHERE length > (SELECT AVG(length) FROM film);
-
-SELECT 
-  a.first_name,
-  a.last_name
-FROM actor AS a
-WHERE a.actor_id IN (
-  SELECT fa.actor_id
-  FROM film_actor AS fa
-  WHERE fa.film_id = (
+WHERE film_id = (
     SELECT film_id
     FROM film
-    WHERE title = 'ALONE TRIP'
-  )
+    WHERE title = 'Hunchback Impossible'
 );
 
+SELECT *
+FROM film
+WHERE length > (
+    SELECT AVG(length)
+    FROM film
+);
+
+SELECT first_name, last_name
+FROM actor
+WHERE actor_id IN (
+    SELECT actor_id
+    FROM film_actor
+    WHERE film_id = (
+        SELECT film_id
+        FROM film
+        WHERE title = 'Alone Trip'
+    )
+);
